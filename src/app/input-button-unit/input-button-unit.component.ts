5f131cc3f455bf3822b7ae25975d5fad863c871b
Ania Wolke <anna@wolke.pl>
2023-11-05 12:40:44 +0100

    NgGirls second part

--- src/app/input-button-unit/input-button-unit.component.ts
+++ src/app/input-button-unit/input-button-unit.component.ts
@@ -10,6 +10,21 @@ import { Component, OnInit } from '@angular/core';
   `,
   styleUrls: ['./input-button-unit.component.scss']
 })
-export class InputButtonUnitComponent {
+export class InputButtonUnitComponent implements OnInit{
   title = 'Hello World';
+
+  changeTitle(newTitle: string) {
+    console.log(newTitle);
+    this.title = newTitle;
+  }
+  constructor() {
+    console.log('in constructor');
+    this.changeTitle('My First Angular App');
+    console.log(this.title);
+  }
+
+  ngOnInit(): void {
+    this.title = 'Angular CLI Rules!';
+    this.changeTitle('My First Angular App');
+  }
 }

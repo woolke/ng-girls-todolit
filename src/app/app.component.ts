bec9d43ae1d8bd127ab07b0af0fc899ff9c3ffba
Ania Wolke <anna@wolke.pl>
2023-11-05 12:22:59 +0100

    NgGirls first part

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,12 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `
+    <h1>
+      Welcome to {{ title }}!
+    </h1>
+    <app-input-button-unit></app-input-button-unit>
+  `,
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
---
+++ src/app/input-button-unit/input-button-unit.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-input-button-unit',
+  template: `
+    <p>
+      input-button-unit works!
+      The title is: {{ title }}
+    </p>
+  `,
+  styleUrls: ['./input-button-unit.component.scss']
+})
+export class InputButtonUnitComponent {
+  title = 'Hello World';
+}

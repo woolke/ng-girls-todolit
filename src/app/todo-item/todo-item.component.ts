b2f7e30098a96d779c19996234fd7164e6f26a02
Ania Wolke <anna@wolke.pl>
2023-11-09 12:26:55 +0100

    NgGirls end

--- src/app/todo-item/todo-item.component.ts
+++ src/app/todo-item/todo-item.component.ts
@@ -4,22 +4,35 @@ import {TodoItem} from "../interfaces/todo-item";
 @Component({
   selector: 'app-todo-item',
   template: `
-    <div class="todo-item">
-      {{ item.title }}
-
-      <button class="btn btn-red" (click)="removeItem()">
-        remove
-      </button>
+    <div>
+      <input type="checkbox"
+             class="todo-checkbox"
+             (click)="completeItem()"
+             [checked]="item.completed"/>
+      <span class="todo-title" [ngClass]="{'todo-complete': item.completed}">
+  {{ item.title }}
+        <button class="btn btn-red" (click)="removeItem()">
+      remove
+    </button>
+</span>
     </div>
   `,
   styleUrls: ['./todo-item.component.scss']
 })
 export class TodoItemComponent {
-  @Output() remove: EventEmitter<TodoItem> = new EventEmitter<TodoItem>();
   @Input() item: TodoItem;
+  @Output() remove: EventEmitter<TodoItem> = new EventEmitter<TodoItem>();
+  @Output() update: EventEmitter<any> = new EventEmitter<any>();
+
+  // put this method below ngOnInit
+  completeItem(): void {
+    this.update.emit({
+      item: this.item,
+      changes: {completed: !this.item.completed}
+    });
+  }
 
   removeItem(): void {
     this.remove.emit(this.item);
   }
-
 }
